6314b6aaf5d7741c8fe8117e420182bbfb39fb5d
NotNekodev <raphael.wegmann81@gmail.com>
2025-04-03 21:32:41 +0200

    feat. SLUB and locking :fire:

--- src/beap.h
+++ src/beap.h
@@ -6,10 +6,18 @@
 #define ALIGN8(size) (((size) + 7) & ~7)
 #define FUNC_PREFIX(x) k##x // so malloc -> kmalloc
 
+// SLUB allocator constants
+#define MAX_SLUB_CACHES 10
+#define MIN_SLUB_SIZE 8
+#define MAX_SLUB_SIZE 2048
+
 typedef void *(*beap_page_alloc_func)();
 typedef void (*beap_page_free_func)(void *ptr);
+typedef void (*beap_lock_func)();
+typedef void (*beap_unlock_func)();
 
-void beap_init(beap_page_alloc_func alloc, beap_page_free_func free);
+void beap_init(beap_page_alloc_func alloc, beap_page_free_func free,
+               beap_lock_func lock_func, beap_unlock_func unlock_func);
 
 void *FUNC_PREFIX(malloc)(size_t size);
 void FUNC_PREFIX(free)(void *ptr);
--- test.c
+++ test.c
@@ -1,41 +1,50 @@
 #include "src/beap.h"
+#include <pthread.h>
 #include <stdio.h>
 #include <sys/mman.h>
 #include <unistd.h>
 
-// Allocate a 4KB page using mmap
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 void *mmap_alloc_page() {
   void *page = mmap(NULL, 4096, PROT_READ | PROT_WRITE,
                     MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  printf("Allocating page at %p\n", page);
   return (page == MAP_FAILED) ? NULL : page;
 }
 
-// Free a 4KB page using munmap
-void mmap_free_page(void *page) { munmap(page, 4096); }
+void test_lock() {
+  printf("Locking...\n");
+  pthread_mutex_lock(&mutex);
+}
+void test_unlock() {
+  printf("Unlocking...\n");
+  pthread_mutex_unlock(&mutex);
+}
+
+void mmap_free_page(void *page) {
+  printf("Freeing page at %p\n", page);
+  munmap(page, 4096);
+}
 
 int main() {
-  // Initialize heap with mmap-based page allocator
-  beap_init(mmap_alloc_page, mmap_free_page);
+  beap_init(mmap_alloc_page, mmap_free_page, test_lock, test_unlock);
 
-  // Allocate memory
   char *str = (char *)kmalloc(16);
   if (str) {
     snprintf(str, 16, "Hello, Heap!");
     printf("%s\n", str);
   }
 
-  // Reallocate memory
   str = (char *)krealloc(str, 32);
   if (str) {
     snprintf(str, 32, "Heap Realloc Works!");
     printf("%s\n", str);
   }
 
-  // Allocate zeroed memory
   int *arr = (int *)kcalloc(4, sizeof(int));
   printf("arr[0] = %d\n", arr[0]);
 
-  // Free memory
   kfree(str);
   kfree(arr);
 
